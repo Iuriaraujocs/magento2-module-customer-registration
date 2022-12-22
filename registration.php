575a86fc6a148538a0a51c039ecf8c8b99c8a731
Iuriaraujocs <iuriaraujoc.eng@gmail.com>
2022-12-22 03:12:57 -0300

    update phpdocs

--- registration.php
+++ registration.php
@@ -1,5 +1,12 @@
 <?php
-
+/**
+ *
+ * @author      Iuri Cardoso Araujo <iuriaraujoc.eng@gmail.com>
+ * @copyright   Copyright (c) 2022, Iuri Cardoso Araujo
+ * @link        https://github.com/iuriaraujocs
+ * @package Iuriaraujocs_Customer
+ *
+ */
 use Magento\Framework\Component\ComponentRegistrar;
 
 ComponentRegistrar::register(ComponentRegistrar::MODULE, 'Iuriaraujocs_Customer', __DIR__);
